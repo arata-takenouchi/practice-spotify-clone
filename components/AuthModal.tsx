0f3a160383f3e05b75c535e96a7e149990360728
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2024-12-20 23:24:02 +0900

    add AuthModal component

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,11 +1,61 @@
 "use client"
 
+import {
+  useSessionContext,
+  useSupabaseClient
+} from "@supabase/auth-helpers-react"
+import { Auth } from "@supabase/auth-ui-react"
+import { useRouter } from "next/navigation"
+import { ThemeSupa } from "@supabase/auth-ui-shared"
+import { useEffect } from "react"
+
+import useAuthModal from "@/hooks/useModal"
+
 import Modal from "./Modal"
 
 const AuthModal = () => {
+  const supabaseClient = useSupabaseClient()
+  const router = useRouter()
+  const { session } = useSessionContext()
+  const { onClose, isOpen } = useAuthModal
+
+  useEffect(() => {
+    if (session) {
+      router.refresh()
+      onClose()
+    }
+  }, [session, router, onClose])
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose()
+    }
+  }
+
   return (
-    <Modal>
+    <Modal
+      title="welcome back"
+      description="login to your account"
+      isOpen={isOpen}
+      onChange={onChange}>
       Auth modal children!
+      <Auth
+        theme="dark"
+        magicLink
+        providers={["github"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: '#404040',
+                brandAccent: '#22c55e'
+              }
+            }
+          }
+        }}
+         />
     </Modal>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,14 @@ import { BiSearch } from "react-icons/bi"
 import { HiHome } from "react-icons/hi"
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { twMerge } from "tailwind-merge"
+import { FaUserAlt } from "react-icons/fa"
+
+import useAuthModal from "@/hooks/useModal"
+import { useUser } from "@/hooks/useUser"
+
 import Button from "./Button"
+import AuthModal from '@/components/AuthModal';
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
 
 interface HeaderProps {
   children: React.ReactNode
@@ -16,9 +23,20 @@ const Header: React.FC<HeaderProps> = ({
   children,
   className
 }) => {
+  const AuthModal = useAuthModal()
   const router = useRouter()
-  const handleLogout = () => {
-    // Handle logout in the future
+
+  const supabaseClient = useSupabaseClient()
+  const { user } = useUser()
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut()
+    // TODO: Reset any playing songs
+    router.refresh()
+
+    if (error) {
+      console.log(error)
+    }
   }
   return (
     <div className={twMerge(`
@@ -59,22 +77,37 @@ const Header: React.FC<HeaderProps> = ({
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div>
+          {user ? (
+            <div className="flex gap-x-4 items-center">
               <Button
-                onClick={() => {}}
-                className="bg-transparent text-neutral-300 font-medium">
-                Sign Up
-              </Button>
-            </div>
-            <div>
-              <Button
-                onClick={() => {}}
+                onClick={handleLogout}
                 className="bg-white px-6 py-2">
-                Log in
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push('/account')}
+                className="bg-white">
+                <FaUserAlt />
               </Button>
             </div>
-          </>
+          ): (
+            <>
+              <div>
+                <Button
+                  onClick={AuthModal.onOpen}
+                  className="bg-transparent text-neutral-300 font-medium">
+                  Sign Up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={AuthModal.onOpen}
+                  className="bg-white px-6 py-2">
+                  Log in
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
