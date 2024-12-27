7193a200b65b3d8585a462b4df3c3b97e56a62bd
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2024-12-27 17:30:45 +0900

    make LikeButton component

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import LikeButton from "@/components/LikeButton"
 import MediaItem from "@/components/MediaItem"
 import { Song } from "@/types"
 
@@ -37,7 +38,7 @@ const SearchContent: React.FC<SearchCOntentProps> = ({
                 onClick={() => {}}
                 data={song} />
             </div>
-            {/* TODO: Add Like Button Here */}
+            <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
