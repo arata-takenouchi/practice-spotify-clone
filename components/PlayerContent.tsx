99d48bdf0c176864d58f5fdb71756e4e97458fb9
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2024-12-28 22:53:39 +0900

    implemented music playback function

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -6,6 +6,10 @@ import LikeButton from '@/components/LikeButton';
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
 
 interface PlayerContentProps {
   song: Song
@@ -16,8 +20,80 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
   song,
   songUrl
 }) => {
-  const Icon = true ? BsPauseFill : BsPlayFill
-  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave
+  const player = usePlayer()
+  const [volume, setVolume] = useState(1)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId)
+    const nextSong = player.ids[currentIndex + 1]
+
+    if (!nextSong) {
+      return player.setId(player.ids[0])
+    }
+
+    player.setId(nextSong)
+  }
+
+  const onPlayPrevious = () => {
+    if (player.ids.length === 0) {
+      return
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId)
+    const previousSong = player.ids[currentIndex - 1]
+
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1])
+    }
+
+    player.setId(previousSong)
+  }
+
+  const [play, { pause, sound }] = useSound(
+    songUrl,
+    {
+      volume: volume,
+      onplay: () => setIsPlaying(true),
+      onend: () => {
+        setIsPlaying(false)
+        onPlayNext()
+      },
+      onpause: () => setIsPlaying(false),
+      format: ['mp3']
+    }
+  )
+
+  useEffect(() => {
+    sound?.play()
+
+    return () => {
+      sound?.unload()
+    }
+  }, [sound])
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      play()
+    } else {
+      pause()
+    }
+  }
+
+  const toggleMute = () => {
+    if (volume === 0) {
+      setVolume(1)
+    } else {
+      setVolume(0)
+    }
+  }
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -37,7 +113,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
           items-center
       ">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="
             h-10
             w-10
@@ -51,6 +127,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
             <Icon size={30} className="text-black" />
         </div>
       </div>
+
       <div
         className="
           hidden
@@ -63,7 +140,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
           gap-x-6
         ">
         <AiFillStepBackward
-          onClick={() => {}}
+          onClick={onPlayPrevious}
           size={30}
           className="
             text-neutral-400
@@ -72,7 +149,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
             transition
           "/>
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="
             flex
             items-center
@@ -87,7 +164,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="
             text-neutral-400
@@ -100,10 +177,12 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       <div className="hidden md:flex w-full justify-end pr-2">
         <div className="flex items-center gap-x-2 w-[120px]">
           <VolumeIcon
-            onClick={() => {}}
+            onClick={toggleMute}
             className="cursor-pointer"
             size={34} />
-          <Slider />
+          <Slider
+            value={volume}
+            onChange={(value) => setVolume(value)} />
         </div>
       </div>
 
